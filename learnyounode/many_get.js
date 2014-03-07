309af795edb53bcd4ce7af617ea109ec509cf6b3
Jake Mays <quaunaut@gmail.com>
2014-03-06 23:41:28 -0600

    Added many_add.js and supporting library.

---
+++ learnyounode/many_get.js
@@ -0,0 +1,14 @@
+var bl = require('bl'),
+    http = require('http');
+
+http.get(process.argv[2], function(request) {
+  request.pipe(bl(function(err, data) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    var result = data.toString();
+    console.log(result.length);
+    console.log(result);
+  }));
+});
